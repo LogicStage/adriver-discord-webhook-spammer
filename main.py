ef1a729f848e22c8bf9bd688bc6c67a445fbfa7a
logik <61352899+LogicStage@users.noreply.github.com>
2021-09-11 01:10:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import requests
+import os
 
 #chuckleszjeb
 print("""
@@ -18,10 +19,12 @@ question = input("Start? [Y/N]")
 if question == 'Y':
     print("Oki hehehe")
     webhook = input(" Enter ur webhook")
+    delay = input(" Enter a delay")
     message = input(" Enter a message")
 
 
-    def spam(message, webhook):
+    def spam(message, webhook, delay):
+        counter = 0
 
 
         while True:
@@ -29,6 +32,7 @@ if question == 'Y':
                 data = requests.post(webhook, json={'content': message})
                 if data.status_code == 204:
                     print(f"message sent elooo {message}")
+                    time.sleep(float(delay))
             except:
                 print("Wrong Webhook :" + webhook)
                 time.sleep(10)
@@ -46,4 +50,4 @@ else:
 
 spammer = 1
 while spammer == 1:
-    spam(message, webhook)
+    spam(message, webhook, delay)
